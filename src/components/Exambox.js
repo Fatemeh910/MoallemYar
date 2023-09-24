2095a5b4f08a3dda512368a5e8aeaa035b58684b
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-25 02:09:46 +0330

    set title and box in component

--- src/components/Exambox.js
+++ src/components/Exambox.js
@@ -2,7 +2,19 @@ import style from "@/components/exambox.module.css"
 
 const Exambox = ()=>{
     return (
-        <div className=""></div>
+        <div className="flex flex-col ">
+            <div className="flex justify-between mx-1 mb-2">
+                <h3>آخرین امتحانات</h3>
+                <h3> مشاهده همه </h3>
+            </div>
+            <div className="flex ">
+            <div className="w-1/5 bg-red-200 justify-around mx-1 rounded-md">test</div>
+            <div className="w-1/5 bg-red-200 justify-around mx-1 rounded-md">test</div>
+            <div className="w-1/5 bg-red-200 justify-around mx-1 rounded-md">test</div>
+            <div className="w-1/5 bg-red-200 justify-around mx-1 rounded-md">test</div>
+            <div className="w-1/5 bg-red-200 justify-around mx-1 rounded-md">test</div>
+            </div>
+        </div>
     )
 }
 
