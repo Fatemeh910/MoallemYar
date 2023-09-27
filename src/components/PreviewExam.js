1df75e5ef4b070bbdf8cf3dba4b4dfae58ebb4c4
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-28 03:04:19 +0330

    set and design link in previewexam

--- src/components/PreviewExam.js
+++ src/components/PreviewExam.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import style from "@/components/previewexam.module.css"
+import style from "@/components/previewexam.module.css";
 
 const PreviewExam = () => {
   return (
@@ -7,9 +7,20 @@ const PreviewExam = () => {
       <h3 className="text-right mr-4 text-textColor"> پیش نمایش</h3>
       <div className="shadow-md">
         <div className=""></div>
-        <Link href="#" className={style.preQuestion} >اتمام سوالات </Link>
-        <Link href="#" className={style.continue} > ثبت و ادامه </Link>
-        <Link href="#" className={style.finishQuestion} > سوالات قبلی </Link>
+        <div className="flex justify-center ">
+          <Link href="#" className={style.finishQuestion}>
+            اتمام سوالات
+            <img src="/assets/icon/tickMark.svg" alt="verticalmenu" />{" "}
+          </Link>
+          <Link href="#" className={style.continue}>
+            <img src="/assets/icon/leftArrowWhite.svg" alt="verticalmenu" /> ثبت
+            و ادامه
+          </Link>
+          <Link href="#" className={style.preQuestion}>
+            سوال قبلی{" "}
+            <img src="/assets/icon/rightArrowWhite.svg" alt="verticalmenu" />{" "}
+          </Link>
+        </div>
       </div>
     </div>
   );
