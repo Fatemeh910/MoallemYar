7192c992fd25532cc6d1d42f98d79e5db8f5e1ee
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-30 10:31:54 +0330

    set wireframe content in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="flex flex-col ">
-    Footer
-  </div>;
+  return (
+    <div className="flex w-screen">
+      <div className="flex flex-col w-1/3 ">
+        <div className="">text</div>
+        <div className="flex flex-col">
+        <Link href="#">1</Link>
+        <Link href="#">2</Link>
+        <Link href="#">3</Link>
+        <Link href="#">4</Link>
+        </div>
+      </div>
+      <div className="flex flex-col w-1/3 ">
+        <div className="">logo</div>
+        <div className="">text</div>
+      </div>
+      <div className="flex flex-col w-1/3 ">
+        <Link href="#">سوالات متداول </Link>
+        <Link href="#">درباره ما</Link>
+        <Link href="#">کد منبع </Link>
+        <Link href="#">لوگو و علائم</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
