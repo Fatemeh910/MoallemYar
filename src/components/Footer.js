cdea3bdb5629c5f5cd2696bef2d2b3fee97706b3
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-30 11:05:42 +0330

    reset links in examarea

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,35 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="flex w-11/12 bg-bgColorFooter text-footerTextColor">
-      <div className="flex flex-col w-1/3 ">
-        <div className="">text</div>
-        <div className="flex flex-col">
-        <Link href="#">1</Link>
-        <Link href="#">2</Link>
-        <Link href="#">3</Link>
-        <Link href="#">4</Link>
-        </div>
+    <div className="flex w-11/12 bg-bgColorFooter text-footerTextColor justify-center itmes-center">
+      <div className="flex flex-col w-1/3 mr-5 ">
+        <Link href="#" className="mt-2">
+          سوالات متداول{" "}
+        </Link>
+        <Link href="#" className="mt-2">
+          درباره ما
+        </Link>
+        <Link href="#" className="mt-2">
+          کد منبع{" "}
+        </Link>
+        <Link href="#" className="mt-2">
+          لوگو و علائم
+        </Link>
       </div>
       <div className="flex flex-col w-1/3 ">
-        <div className="">logo</div>
+
         <div className="">text</div>
       </div>
       <div className="flex flex-col w-1/3 text-right">
-        <Link href="#">سوالات متداول </Link>
-        <Link href="#">درباره ما</Link>
-        <Link href="#">کد منبع </Link>
-        <Link href="#">لوگو و علائم</Link>
+        <div className="">text</div>
+        <div className="flex flex-col">
+          <Link href="#">1</Link>
+          <Link href="#">2</Link>
+          <Link href="#">3</Link>
+          <Link href="#">4</Link>
+        </div>
       </div>
     </div>
   );
