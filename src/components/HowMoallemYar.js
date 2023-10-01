68dc20d6d07c2028ddb4099630679e5314c261e3
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-01 19:38:29 +0330

    reset title howmoallemyar component

--- src/components/HowMoallemYar.js
+++ src/components/HowMoallemYar.js
@@ -1,7 +1,24 @@
 const HowMoallemYar = () => {
-  return <div className="  ">
-
-  </div>;
+  return (
+    <div className="w-screen flex flex-col   ">
+      <div className="flex flex-col">
+        <div className="flex justify-center">
+          <h1>معلم یار چیست ؟</h1>
+        </div>
+        <div className="flex justify-center mt-6">
+          <h3 className="text-center">
+            طراحی برگه امتحانی ، همواره یکی از دغدغه های معلمان در طول ترم
+            تحصیلی خصوصا پایان ترم بوده است .ما در سامانه آموزشیار تلاش کرده ایم
+            فضایی را برای شما فراهم
+            <br /> آوریم که در عین سادگی ،امتحان های خودرا در سریعترین زمان ممکن
+            طراحی کنید.
+          </h3>
+        </div>
+      </div>
+      <div className="">2</div>
+      <div className="">3</div>
+    </div>
+  );
 };
 
 export default HowMoallemYar;
