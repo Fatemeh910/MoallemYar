f8d5809312194cf5dddcef4b77c6292a08819e30
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-02 10:43:14 +0330

    create MainPage.module.css and import it

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,8 +1,13 @@
-
-
+import MainPage from "@/components/Mainpage.module.css"
 const MainPage = () => {
   return (
-    <div className="">
+    <div className="flex flex-col w-11/12 float-left">
+      <div className="w-screen">
+        <h3 className="font-bold text-xl">
+          دیگر نیاز نیست زمان خود را طرف <br />
+          طراحی برگه‌امتحانی کنید!
+        </h3>
+      </div>
     </div>
   );
 };
