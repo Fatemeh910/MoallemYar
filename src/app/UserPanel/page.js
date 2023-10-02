9264a0d32993d53b15d9f7c1c42451339e902408
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-03 00:47:00 +0330

    fix bugs

--- src/app/UserPanel/page.js
+++ src/app/UserPanel/page.js
@@ -2,16 +2,16 @@ import Examarea from "@/components/Examarea";
 import PreviewExam from "@/components/PreviewExam";
 import Footer from "@/components/Footer";
 import HowMoallemYar from "@/components/HowMoallemYar";
-// import MainPage from "@/components/MainPage";
+import MainPage from "@/components/MainPage";
 
 const UserPanelDashboard = () => {
   return (
     <div className="felx">
-      {/* <Examarea />
-      <PreviewExam />
-      <Footer /> */}
+      {/* <Examarea /> */}
+      {/* <PreviewExam /> */}
       {/* <HowMoallemYar /> */}
       <MainPage />
+      {/* <Footer /> */}
     </div>
   );
 };
