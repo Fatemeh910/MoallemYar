3705df669ffcb8fbb0ed8beb3af3571ae84beb47
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-02 12:35:30 +0330

    set image background and style for title

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,12 +1,17 @@
-import MainPage from "@/components/Mainpage.module.css"
+import Image from "next/image";
+import style from "@/components/Mainpage.module.css"
+
 const MainPage = () => {
   return (
     <div className="flex flex-col w-11/12 float-left">
-      <div className="w-screen">
-        <h3 className="font-bold text-xl">
+      <div className="">
+        <h3 className="text-textColor font-bold text-xl mb-8 mt-4">
           دیگر نیاز نیست زمان خود را طرف <br />
           طراحی برگه‌امتحانی کنید!
         </h3>
+      </div>
+      <div className={style.image}>
+      <Image src="/assets/img/wallpaper.jpg" width={900} height={300}  alt="background image" style={{borderRadius:15}}/>
       </div>
     </div>
   );
