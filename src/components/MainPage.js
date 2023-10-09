913264b622f7b5ae1787a306741c95fec83a9b1c
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-09 15:24:42 +0330

    npm i

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -20,7 +20,30 @@ const MainPage = () => {
           className={style.Image}
         />
       </div>
-      <div className={style.txtBox}><h3>تنها با ۵ مرحله ساده آزمون خودرا طراحی <br/>کنید و خروجی PDF آن را دریافت کنید.<br/>همچنین می توانید آزمون خودرا بصورت <br/>آنلاین برگزار کنید !</h3></div>
+      <div className={style.txtBox}>
+        <img
+          src="/assets/icon/pinIcon.svg"
+          style={{ width: 15 }}
+          alt=""
+          className={style.pinIcon}
+        />
+        <h3 className="text-textColor mb-4 text-sm  font-semibold ">
+          تنها با ۵ مرحله ساده آزمون خودرا طراحی <br />
+          کنید و خروجی PDF آن را دریافت کنید.
+          <br />
+          همچنین می توانید آزمون خودرا بصورت <br />
+          آنلاین برگزار کنید !
+        </h3>
+        <Link href="#" className={style.btn}>
+          همین حالا شروع کنید!
+          <img
+            src="/assets/icon/shevronSmalLeft.svg"
+            style={{ width: 15 }}
+            alt=""
+            className={style.leftArrowIcon}
+          />
+        </Link>
+      </div>
     </div>
   );
 };
