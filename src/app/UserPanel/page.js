9495510d35b4a06699431ba1faed6e447b89ac22
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-09 18:56:02 +0330

    Temporary extracted other components for pull request

--- src/app/UserPanel/page.js
+++ src/app/UserPanel/page.js
@@ -1,17 +1,11 @@
 import Examarea from "@/components/Examarea";
-import PreviewExam from "@/components/PreviewExam";
-import Footer from "@/components/Footer";
-import HowMoallemYar from "@/components/HowMoallemYar";
-import MainPage from "@/components/MainPage";
+
 
 const UserPanelDashboard = () => {
   return (
     <div className="felx">
       <Examarea />
-      <PreviewExam />
-      <HowMoallemYar />
-      <MainPage />
-      <Footer />
+
     </div>
   );
 };
