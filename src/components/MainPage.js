6a2054c46be5859766a0551bdfffbc26edbc100e
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-09 13:39:21 +0330

    create text box

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,19 +1,25 @@
 import Image from "next/image";
-
+import style from "@/components/mainPage.module.css";
 
 const MainPage = () => {
   return (
-    <div className="flex flex-col w-11/12 float-left">
+    <div className="flex flex-col w-11/12 float-left relative">
       <div className="">
         <h3 className="text-textColor font-bold text-xl mb-8 mt-4">
           دیگر نیاز نیست زمان خود را طرف <br />
           طراحی برگه‌امتحانی کنید!
         </h3>
       </div>
-      <div className="">
-      <Image src="/assets/img/wallpaper.jpg" width={900} height={300}  alt="background image" style={{borderRadius:15}}/>
+      <div className="" dir="ltr">
+        <Image
+          src="/assets/img/wallpaper.jpg"
+          width={900}
+          height={300}
+          alt="background image"
+          className={style.Image}
+        />
       </div>
-     
+      <div className=""><h3>تنها با ۵ مرحله ساده آزمون خودرا طراحی <br/>کنید و خروجی PDF آن را دریافت کنید.<br/>همچنین می توانید آزمون خودرا بصورت <br/>آنلاین برگزار کنید !</h3></div>
     </div>
   );
 };
