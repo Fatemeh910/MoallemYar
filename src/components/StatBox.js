117ac2f2df1a9082896c127a5a42bde55507e70d
fatemeh <fatemehkhoshnod91@gmail.com>
2023-10-12 19:00:26 +0330

    set icon

--- src/components/StatBox.js
+++ src/components/StatBox.js
@@ -1,7 +1,7 @@
 import Box from "./Box"
 const StatBox = (props) => {
     return (
-            <div className="w-full md:bg-slate-600 md:w-full  bg-red-500 md:flex md:flex-row lg:bg-blue-600  items-center justify-center md:rounded-2xl lg:w-full lg:flex lg:flex-row gap-3 p-3 ">
+            <div className="w-full md:bg-slate-600 md:w-full  bg-red-500 md:flex md:flex-row lg:bg-blue-600  items-center justify-center md:rounded-2xl lg:w-full lg:flex lg:flex-row gap-3 p-4 ">
                 <div className="lg:flex lg:flex-row lg:w-full gap-2 md:w-full">
 
                 <Box className=" lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#29d4A8" endcolor="#3677E2" iconUrl="/assets/icon/calender.svg"/>
