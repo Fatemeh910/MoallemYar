0a4183af95c68e93a8797f217f26bda1a4bf4e12
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-12 14:38:52 +0330

    fix one bug in topbar component

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Avatar, Popover, Button } from "antd";
-import Link from "next/link";
+import { Avatar, Popover } from "antd";
 
 export default function Topbar() {
   const [open, setOpen] = useState(false);
@@ -11,10 +10,11 @@ export default function Topbar() {
   const handleOpenChange = (newOpen) => {
     setOpen(newOpen);
   };
+
   return (
     <div className="flex w-full bg-white rounded-full mt-2 p-2  align-center justify-between">
       <div className="">
-        <div className="flex">
+        <div className="flex items-center">
           <img
             src="/assets/icon/homeOutline.svg"
             alt="homeIcon"
@@ -27,16 +27,22 @@ export default function Topbar() {
         <p style={{ color: "#555555" }}> علی امیری ، خوش آمدید </p>
         <Popover
           content={
-            <a onClick={hide} className="flex align-center pb-3 pr-2
-            pl-2">
+            <a
+              onClick={hide}
+              className="flex align-center pb-3 pr-2
+            pl-2"
+            >
               <img src="/assets/icon/powerButton.svg" alt="powerButton" />
               <p style={{ color: "#EE6E62", fontSize: 17 }}>خروج</p>
             </a>
           }
           title={
             <div className="flex hover:w-full hover:p-0">
-              <a href="#" className="flex p-2 hover:bg-slate-100 hover:w-full hover:h-full
-              " >
+              <a
+                href="#"
+                className="flex p-2 hover:bg-slate-100 hover:w-full hover:h-full
+              "
+              >
                 <img src="/assets/icon/userOutline.svg" alt="userOutline" />
                 <p className="mr-2">پروفایل کاربری</p>
               </a>
@@ -45,7 +51,6 @@ export default function Topbar() {
           trigger="click"
           open={open}
           onOpenChange={handleOpenChange}
-          
         >
           <Avatar size={30} className="mr-6 " />
         </Popover>
