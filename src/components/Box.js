138aa2879bc2864c0a7d2334bedfb99ae4ba8af6
fatemeh <fatemehkhoshnod91@gmail.com>
2023-10-12 19:39:41 +0330

    set color

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,8 +1,8 @@
 import style from "./box.module.css"
 const Box =(props) =>{
     return(
-        <div className={"w-full  md:flex-col   lg:w-full text-white relative lg:flex lg:items-center lg:justify-center sm:items-center sm:justify-center  rounded-2xl     "  +style.item}  style={{ background: "linear-gradient(250.38deg," + props.startcolor + " 4.14%," + props.endcolor + " 97.36%)"  }}>
-                <div className=" w-full flex items-center flex-col   ">
+        <div className={"w-full p-4 md:flex-col   lg:w-full text-white relative lg:flex lg:items-center lg:justify-center sm:items-center sm:justify-center  rounded-2xl     "  +style.item}  style={{ background: "linear-gradient(250.38deg," + props.startcolor + " 4.14%," + props.endcolor + " 97.36%)"  }}>
+                <div className=" w-full flex items-center flex-col  ">
                     <h3>{props.title}</h3>
                     <p>{props.topic}</p>
                     <img className={style.icon} alt=''  src={props.iconUrl}  />
