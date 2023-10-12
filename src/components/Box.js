3d0ec455f6af3396468083c805a7a8babb3c0050
fatemeh <fatemehkhoshnod91@gmail.com>
2023-10-12 18:19:30 +0330

    compail respansive

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,11 @@
 import style from "./box.module.css"
 const Box =(props) =>{
     return(
-        <div className={"w-full  md:flex-col   lg:w-full text-white  relative lg:flex lg:items-center lg:justify-center sm:items-center sm:justify-center  rounded-xl   "  +style.item}  style={{ background: "linear-gradient(250.38deg," + props.startcolor + " 4.14%," + props.endcolor + " 97.36%)"  }}>
+        <div className={"w-full  md:flex-col   lg:w-full text-white  relative lg:flex lg:items-center lg:justify-center sm:items-center sm:justify-center  rounded-2xl   "  +style.item}  style={{ background: "linear-gradient(250.38deg," + props.startcolor + " 4.14%," + props.endcolor + " 97.36%)"  }}>
                 <div className=" w-full flex items-center flex-col mt-4  ">
                     <h3>{props.title}</h3>
                     <p>{props.topic}</p>
-                    <img alt=''  src={props.iconUrl}  />
+                    <img className={style.icon} alt=''  src={props.iconUrl}  />
                 </div>
             </div>
     )
--- src/components/StatBox.js
+++ src/components/StatBox.js
@@ -1,16 +1,16 @@
 import Box from "./Box"
 const StatBox = (props) => {
     return (
-            <div className="w-full md:bg-slate-600 md:w-full  bg-red-500 md:flex md:flex-row lg:bg-blue-600  items-center justify-center md:rounded-lg lg:w-full lg:flex lg:flex-row gap-3 p-3 ">
+            <div className="w-full md:bg-slate-600 md:w-full  bg-red-500 md:flex md:flex-row lg:bg-blue-600  items-center justify-center md:rounded-2xl lg:w-full lg:flex lg:flex-row gap-3 p-3 ">
                 <div className="lg:flex lg:flex-row lg:w-full gap-2 md:w-full">
 
                 <Box className=" lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#29d4A8" endcolor="#3677E2" iconUrl="/assets/icon/calender.svg"/>
-                <Box className=" lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#29d4A8" endcolor="#3677E2" iconUrl="/assets/icon/calender.clock.svg"/>
+                <Box className=" lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#c169D6" endcolor="#643BBD" iconUrl="/assets/icon/calender.clock.svg"/>
                 </div>
 
                 <div className="lg:flex lg:flex-row lg:w-full gap-2 md:w-full">
-                <Box className="lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#29d4A8" endcolor="#3677E2" iconUrl="/assets/icon/user-plus.svg"/>
-                <Box className="lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#29d4A8" endcolor="#3677E2" iconUrl="/assets/icon/clock2.svg"/>
+                <Box className="lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#FA2A87" endcolor="#F939CB" iconUrl="/assets/icon/user-plus.svg"/>
+                <Box className="lg:w-full lg:flex lg:flex-row" title="امتحانات دانش اموزان" topic="32" startcolor="#F8C274" endcolor="#EE6E62" iconUrl="/assets/icon/clock2.svg"/>
                 </div>
             </div>
     )
