27a77e3cafbfb6c418760f396c0d8b5475558c11
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-19 01:04:55 +0330

    create TestAnswer component and import it

--- src/app/TestDesign/page.js
+++ src/app/TestDesign/page.js
@@ -1,7 +1,9 @@
-export default function  TestDesign(){
-    return(
-        <>
-        test design
-        </>
-    )
+import TestAnswer from "@/components/TestAnswer";
+
+export default function TestDesign() {
+  return (
+    <>
+    <TestAnswer />
+    </>
+  )
 }
