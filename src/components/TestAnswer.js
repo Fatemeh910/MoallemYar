cb137e6cabaf8dce1860e5022442b352dc64e990
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-19 01:35:22 +0330

    set text area input in TestAnswer component

--- src/components/TestAnswer.js
+++ src/components/TestAnswer.js
@@ -1,7 +1,31 @@
-export default ()=>{
-    return (
-        <div className="">
-            TestAnswer
-        </div>
-    )
-}
+"use client";
+import React from "react";
+import { ConfigProvider, Input } from "antd";
+
+export default () => {
+  const { TextArea } = Input;
+  const onChange = (e) => {
+    console.log("Change:", e.target.value);
+  };
+  return (
+    <div className="">
+      <ConfigProvider
+        theme={{
+          token: {
+            borderRadius: 1,
+          },
+        }}
+      >
+        <TextArea
+          showCount
+          maxLength={100}
+          style={{
+            height: 60,
+          }}
+          onChange={onChange}
+          placeholder="پاسخ سوال رااینجابنویسید."
+        />
+      </ConfigProvider>
+    </div>
+  );
+};
