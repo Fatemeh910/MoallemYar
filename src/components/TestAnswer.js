642779d93b11a6d3aada77fbc33d2aeb4a812af4
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-19 13:10:05 +0330

    Creating the basic structure of the single-answer question @Ahmadbeyranvand

--- src/components/TestAnswer.js
+++ src/components/TestAnswer.js
@@ -5,44 +5,41 @@ const { CheckableTag } = Tag;
 
 export default () => {
   const { TextArea } = Input;
-  const tagsData = [
-    "پاسخ",
-    "سوال",
-    "اینجا",
-    "بنویسید",
-    "پاسخ",
-    "سوال",
-    "اینجا",
-    "بنویسید",
-  ];
-  const [selectedTags, setSelectedTags] = useState(["Books"]);
+  const [tagsData, setTagsData] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [shortAnswerSentence, setShortAnswerSentence] = useState("");
+
+  const convertToTags = (e) => {
+    setShortAnswerSentence(e.target.value);
+    let replacedToComma = shortAnswerSentence.replaceAll(" ", "،");
+    let seperated = replacedToComma.split("،").filter((item) => {
+      return item != "";
+    });
+    setTagsData(seperated);
+  };
+
   const handleChange = (tag, checked) => {
     const nextSelectedTags = checked
       ? [...selectedTags, tag]
       : selectedTags.filter((t) => t !== tag);
-    console.log("You are interested in: ", nextSelectedTags);
     setSelectedTags(nextSelectedTags);
   };
 
-  const [shortAnswerSentence, setShortAnswerSentence] = useState("");
   return (
     <div className="">
       <TextArea
         value={shortAnswerSentence}
-        onChange={(e) => {
-          setShortAnswerSentence(e.target.value);
-        }}
+        onChange={convertToTags}
         showCount
         maxLength={100}
         rows={1}
         placeholder=" پاسخ سوال خود را اینجا واردکنید. "
       />
-      <div className="mb-2 ">
-        <span style={{}}></span>
+      <div className="mt-2 ">
         <Space size={[0, 8]} wrap>
-          {tagsData.map((tag) => (
+          {tagsData.map((tag, index) => (
             <CheckableTag
-              key={tag}
+              key={"tag_" + tag + "_" + index}
               checked={selectedTags.includes(tag)}
               onChange={(checked) => handleChange(tag, checked)}
               style={{ color: "#555555" }}
