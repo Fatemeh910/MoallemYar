fbfb9a516075c93e26b522e789d161f6aafbec2f
“Fatemeh910” <fatemehkhoshnod91@gmail.com>
2023-10-24 15:44:04 +0330

    Set input

--- src/components/Questionsubject/QuestionSubject.js
+++ src/components/Questionsubject/QuestionSubject.js
@@ -1,8 +1,19 @@
-const QuestionSubject =() =>{
-    return(
-        <div className="w-full flex bg-red-100">
-        <h2>عنوان سوال</h2>
-        <p>بارم (نمره)</p>
+import React from 'react';
+import { Input } from 'antd';
+const QuestionSubject = () => {
+    return (
+        <div className="w-full flex bg-red-100 mt-10">
+            <div className='gap-4 flex flex-row'>
+                <div className=''>
+                    <h2>عنوان سوال</h2>
+                    <Input size="large" placeholder="لطفا اینجا چیزی ننویسید..." />
+                </div>
+                <div>
+                    <p>بارم (نمره)</p>
+                    <Input placeholder='برای مثال' />
+                </div>
+            </div>
+
         </div>
     )
 }
