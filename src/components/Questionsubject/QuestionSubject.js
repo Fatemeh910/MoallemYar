fd2cdf6b986e4dc25d61106f8a347ce8f70e183f
“Fatemeh910” <fatemehkhoshnod91@gmail.com>
2023-10-24 16:01:20 +0330

    set tag p

--- src/components/Questionsubject/QuestionSubject.js
+++ src/components/Questionsubject/QuestionSubject.js
@@ -2,19 +2,30 @@ import React from 'react';
 import { Input } from 'antd';
 const QuestionSubject = () => {
     return (
-        <div className="w-full flex bg-red-100 mt-10">
+      
+        <div className="w-full flex flex-col bg-red-100 mt-10 gap-4 p-3">
             <div className='gap-4 flex flex-row'>
                 <div className=''>
                     <h2>عنوان سوال</h2>
                     <Input size="large" placeholder="لطفا اینجا چیزی ننویسید..." />
                 </div>
-                <div>
+                <div className='mr-auto'>
                     <p>بارم (نمره)</p>
-                    <Input placeholder='برای مثال' />
+                    <Input placeholder=' 2:برای مثال'   />
                 </div>
             </div>
 
+
+
+
+            <div className='flex '>
+            <p> نوع سوال</p>
+            <p> تصویرمربوطه</p>
+            
+            </div>
+
         </div>
+  
     )
 }
 export default QuestionSubject
