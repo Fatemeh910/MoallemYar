f5addcf22c91812f929f2d320d0bfb170ecadebb
“Fatemeh910” <fatemehkhoshnod91@gmail.com>
2023-10-25 13:19:13 +0330

    set color input

--- src/components/Questionsubject/QuestionSubject.js
+++ src/components/Questionsubject/QuestionSubject.js
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+import taky from '../../../public/assets/img/icons/trueFalse.png';
 import React from 'react';
 import { Input } from 'antd';
 const QuestionSubject = () => {
     return (
-      
+
         <div className="w-full flex flex-col bg-red-100 mt-10 gap-4 p-3">
             <div className='gap-4 flex flex-row'>
                 <div className=''>
@@ -11,21 +13,54 @@ const QuestionSubject = () => {
                 </div>
                 <div className='mr-auto'>
                     <p>بارم (نمره)</p>
-                    <Input placeholder=' 2:برای مثال'   />
+                    <Input placeholder=' 2:برای مثال' />
                 </div>
             </div>
 
 
 
 
-            <div className='flex '>
-            <p> نوع سوال</p>
-            <p> تصویرمربوطه</p>
-            
+            <div className='flex flex-col'>
+                <div className='flex '>
+
+                    <p> نوع سوال</p>
+                    <p> تصویرمربوطه</p>
+                </div>
+                <div className='bg-white flex mt-5 border border-inherit rounded-2xl items-center justify-center'>
+
+                    <div className=' border border-l-purple-300'>
+                        <Image width={60} alt='aks cc' src={taky} />
+                        <p>تک کلمه ای</p>
+                    </div>
+                    <div>
+                        <Image width={60} alt='aks cc' src={taky} />
+                        <p>تک کلمه ای</p>
+                    </div>
+                    <div>
+                        <Image width={60} alt='aks cc' src={taky} />
+                        <p>تک کلمه ای</p>
+                    </div>
+                    <div>
+                        <Image width={60} alt='aks cc' src={taky} />
+                        <p>تک کلمه ای</p>
+                    </div>
+                    <div>
+                        <Image width={60} alt='aks cc' src={taky} />
+                        <p>تک کلمه ای</p>
+                    </div>
+
+                </div>
+
             </div>
 
+
+
+
+
+
+
         </div>
-  
+
     )
 }
 export default QuestionSubject
